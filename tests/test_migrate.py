181e9fb30d929fc33281a9e296849936e84ec6b1
Alexandre Bulté <alexandre@bulte.net>
2024-09-11 07:54:52 +0200

    review: use content instead of dates for cmp

--- tests/test_migrate.py
+++ tests/test_migrate.py
@@ -1,35 +1,29 @@
-from datetime import datetime
-
 from conftest import Fixture
+from lxml import etree
 from test_transform import get_transform_results
 
-from ecospheres_migrator.geonetwork import extract_record_info
 from ecospheres_migrator.migrator import Migrator
 
 
-def get_change_dates(migrator: Migrator, md_fixtures: list[Fixture]) -> dict[str, datetime]:
-    sources = migrator.gn.get_sources()
-    change_dates = {}
+def get_records(migrator: Migrator, md_fixtures: list[Fixture]) -> dict[str, str]:
+    records = {}
     for fixture in md_fixtures:
         record = migrator.gn.get_record(fixture.uuid)
-        record_info = extract_record_info(record, sources)
-        change_date = record_info.xpath(".//changeDate")
-        change_dates[fixture.uuid] = datetime.fromisoformat(change_date[0].text)
-    return change_dates
+        records[fixture.uuid] = (etree.tostring(record),)
+    return records
 
 
 def test_migrate_noop_overwrite(migrator: Migrator, md_fixtures: list[Fixture]):
-    """`noop` migration in overwrite mode should update change date"""
-    change_dates_before = get_change_dates(migrator, md_fixtures)
+    """`noop` migration in overwrite mode should update content"""
+    records_before = get_records(migrator, md_fixtures)
 
     batch, _ = get_transform_results("noop", migrator)
     migrator.migrate(batch, overwrite=True, group=None)
 
-    change_dates_after = get_change_dates(migrator, md_fixtures)
+    records_after = get_records(migrator, md_fixtures)
 
     for uuid in [f.uuid for f in md_fixtures]:
-        # TODO: maybe compare on something else, precision is only to the second
-        assert change_dates_after[uuid] > change_dates_before[uuid]
+        assert records_after[uuid] != records_before[uuid]
 
 
 def test_migrate_noop_duplicate(
@@ -39,16 +33,16 @@ def test_migrate_noop_duplicate(
     `noop` migration in duplicate mode should create new records in specific group.
     Use `clean_md_fixtures` to remove all records from the group before running the test.
     """
-    change_dates_before = get_change_dates(migrator, clean_md_fixtures)
+    records_before = get_records(migrator, clean_md_fixtures)
 
     batch, _ = get_transform_results("noop", migrator)
     migrator.migrate(batch, overwrite=False, group=group_fixture)
 
-    change_dates_after = get_change_dates(migrator, clean_md_fixtures)
+    records_after = get_records(migrator, clean_md_fixtures)
 
-    # dates have not changed on original records
+    # content has not changed on original records
     for uuid in [f.uuid for f in clean_md_fixtures]:
-        assert change_dates_after[uuid] == change_dates_before[uuid]
+        assert records_after[uuid] == records_before[uuid]
 
     # new records have been created in the test group
     records = migrator.gn.get_records(query={"facet.q": f"groupOwner/{group_fixture}"})
@@ -56,16 +50,16 @@ def test_migrate_noop_duplicate(
 
 
 def test_migrate_error_overwrite(migrator: Migrator, md_fixtures: list[Fixture]):
-    """`error` migration in overwrite mode should not touch change date"""
-    change_dates_before = get_change_dates(migrator, md_fixtures)
+    """`error` migration in overwrite mode should not touch content"""
+    records_before = get_records(migrator, md_fixtures)
 
     batch, _ = get_transform_results("error", migrator)
     migrator.migrate(batch, overwrite=True, group=None)
 
-    change_dates_after = get_change_dates(migrator, md_fixtures)
+    records_after = get_records(migrator, md_fixtures)
 
     for uuid in [f.uuid for f in md_fixtures]:
-        assert change_dates_after[uuid] == change_dates_before[uuid]
+        assert records_after[uuid] == records_before[uuid]
 
 
 def test_migrate_error_duplicate(
@@ -75,16 +69,16 @@ def test_migrate_error_duplicate(
     `error` migration in duplicate mode should not create records or touch existing ones.
     Use `clean_md_fixtures` to remove all records from the group before running the test.
     """
-    change_dates_before = get_change_dates(migrator, clean_md_fixtures)
+    records_before = get_records(migrator, clean_md_fixtures)
 
     batch, _ = get_transform_results("error", migrator)
     migrator.migrate(batch, overwrite=False, group=group_fixture)
 
-    change_dates_after = get_change_dates(migrator, clean_md_fixtures)
+    records_after = get_records(migrator, clean_md_fixtures)
 
-    # dates have not changed on original records
+    # content has not changed on original records
     for uuid in [f.uuid for f in clean_md_fixtures]:
-        assert change_dates_after[uuid] == change_dates_before[uuid]
+        assert records_after[uuid] == records_before[uuid]
 
     # no records have been created in the test group
     records = migrator.gn.get_records(query={"facet.q": f"groupOwner/{group_fixture}"})
